384c7cbc9676db693703f8d711143f9f91dd8fbd
leylaheydarova <lbadalzade@gmail.com>
2025-06-18 14:57:21 +0400

    First set of env

---
+++ Setup/Setup.App/Program.cs
@@ -0,0 +1,34 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+DotNetEnv.Env.Load();
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+var apiKey = Environment.GetEnvironmentVariable("DB_SECRET");
+
+Console.WriteLine($"Running in: {environment}");
+
+Console.WriteLine($"Using API Key: {apiKey}");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
